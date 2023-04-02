b9f188e5319689c0ef0a1c166f61f9e68415f040
Michael <michaeltenkorang415@gmail.com>
2023-04-02 15:21:57 -0400

    Methods almost complete

--- BSTMap.java
+++ BSTMap.java
@@ -76,11 +76,29 @@ public class BSTMap<K, V> implements MapSet<K, V> {
 
     @Override
     public boolean containsKey(K key) {
-        if (key == null){
+        if (key == null) {
             return false;
         }
 
-        if ()
+        return containsKey(key, root);
+    }
+
+    private boolean containsKey(K key, Node<K, V> curNode) {
+        if (comparator.compare(key, curNode.getKey()) > 0) {
+            if (curNode.right != null) {
+                return containsKey(key, curNode.right);
+            } else {
+                return false;
+            }
+        } else if (comparator.compare(key, curNode.getKey()) < 0) {
+            if (curNode.left != null) {
+                return containsKey(key, curNode.left);
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -110,6 +128,24 @@ public class BSTMap<K, V> implements MapSet<K, V> {
         }
     }
 
+    private Node<K, V> getNode(K key, Node<K, V> cur) {
+        if (comparator.compare(key, cur.getKey()) < 0) {
+            if (cur.left != null) {
+                return getNode(key, cur.left);
+            } else {
+                return null;
+            }
+        } else if (comparator.compare(key, cur.getKey()) > 0) {
+            if (cur.right != null) {
+                return getNode(key, cur.right);
+            } else {
+                return null;
+            }
+        } else {
+            return cur;
+        }
+    }
+
     @Override
     public V remove(K key) {
         // TODO Auto-generated method stub
@@ -118,20 +154,68 @@ public class BSTMap<K, V> implements MapSet<K, V> {
 
     @Override
     public ArrayList<K> keySet() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keySet'");
+        if (size() == 0) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<K> output = new ArrayList<>();
+        return keySet(root, output);
+    }
+
+    private ArrayList<K> keySet(Node<K, V> curNode, ArrayList<K> output) {
+        if (curNode == null) {
+            return null;
+        }
+
+        keySet(curNode.left, output);
+        output.add(curNode.getKey());
+        keySet(curNode.right, output);
+
+        return output;
     }
 
     @Override
     public ArrayList<V> values() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'values'");
+        if (size() == 0) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<V> output = new ArrayList<>();
+        return values(root, output);
+    }
+
+    private ArrayList<V> values(Node<K, V> curNode, ArrayList<V> output) {
+        if (curNode == null) {
+            return null;
+        }
+
+        values(curNode.left, output);
+        output.add(curNode.getValue());
+        values(curNode.right, output);
+
+        return output;
     }
 
     @Override
-    public ArrayList<MapSet.KeyValuePair<K, V>> entrySet() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'entrySet'");
+    public ArrayList<KeyValuePair<K, V>> entrySet() {
+        if (size() == 0) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<KeyValuePair<K, V>> output = new ArrayList<>();
+        return entrySet(root, output);
+    }
+
+    private ArrayList<KeyValuePair<K, V>> entrySet(Node<K, V> curNode, ArrayList<KeyValuePair<K, V>> output) {
+        if (curNode == null) {
+            return null;
+        }
+
+        entrySet(curNode.left, output);
+        output.add(new KeyValuePair<K, V>(curNode.getKey(), curNode.getValue()));
+        entrySet(curNode.right, output);
+
+        return output;
     }
 
     @Override
@@ -147,7 +231,46 @@ public class BSTMap<K, V> implements MapSet<K, V> {
 
     @Override
     public int maxDepth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'maxDepth'");
+        if (size == 0) {
+            return 0;
+        }
+
+        int curDepth = 0;
+        return maxDepth(root, ++curDepth, ++curDepth);
+    }
+
+    private int maxDepth(Node<K, V> curNode, int curDepth, int curMax) {
+        if (curNode == null) {
+            curDepth--;
+
+            if (curDepth > curMax) {
+                curMax = curDepth;
+            }
+
+            return curMax;
+        }
+
+        int leftMax = maxDepth(curNode.left, ++curDepth, curMax);
+        curDepth--;
+        int rightMax = maxDepth(curNode.right, ++curDepth, curMax);
+
+        return Math.max(leftMax, rightMax);
+    }
+
+    public String toString() {
+
+    }
+
+    public static void main(String[] args) {
+        BSTMap<Integer, String> myTree = new BSTMap<>();
+
+        myTree.put(4, "Michael");
+        myTree.put(2, "Tracy");
+        myTree.put(3, "Vanessa");
+        myTree.put(1, "Vanessa");
+        myTree.put(6, "Vanessa");
+        myTree.put(5, "Vanessa");
+
+        System.out.println(myTree.maxDepth());
     }
 }
